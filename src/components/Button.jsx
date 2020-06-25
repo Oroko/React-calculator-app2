6340107a68b0fe5a6de7f1f08eea382115b3dd0a
Oroko <aoroko09@gmail.com>
2020-06-25 16:12:44 -0400

    Added styles on button.css and app.css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Button.css";
 class Button extends Component {
   render() {
-    return <div className="">{this.props.children}</div>;
+    return <div className="button">{this.props.children}</div>;
   }
 }
 
