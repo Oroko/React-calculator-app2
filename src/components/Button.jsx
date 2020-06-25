749651929762e90dfaa313f29e2ce52780cd082d
Oroko <aoroko09@gmail.com>
2020-06-25 16:55:07 -0400

    Added a function isOperator to change the color of the operators buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 import "./Button.css";
 class Button extends Component {
+  isOperator = (val) => {
+    return !isNaN(val) || val === "." || val === "=";
+  };
+
   render() {
-    return <div className="button">{this.props.children}</div>;
+    return (
+      <div
+        className={`button ${
+          this.isOperator(this.props.children) ? "" : "operator"
+        }`}
+      >
+        {this.props.children}
+      </div>
+    );
   }
 }
 
