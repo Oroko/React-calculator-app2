9fe3401078c1e5bf98e1f5e5a1ce60f5dd5f211d
Oroko <aoroko09@gmail.com>
2020-06-25 22:33:28 -0400

    Added the addition functionality and the Evaluate(equal)

--- src/App.js
+++ src/App.js
@@ -37,6 +37,20 @@ class App extends Component {
   clearInput = () => {
     this.setState({ input: "" });
   };
+
+  add = () => {
+    this.state.previousNumber = this.state.input;
+    this.setState({ input: "" });
+    this.state.operator = "plus";
+  };
+  evaluate = () => {
+    this.state.currentNumber = this.state.input;
+    if(this.state.operator == "plus"){
+      this.setState({
+        input: parseInt(this.state.previousNumber) + parseInt(this.state.currentNumber)
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -60,13 +74,13 @@ class App extends Component {
             <Button handleClick={this.addToInput}>1</Button>
             <Button handleClick={this.addToInput}>2</Button>
             <Button handleClick={this.addToInput}>3</Button>
-            <Button handleClick={this.addToInput}>+</Button>
+            <Button handleClick={this.add}>+</Button>
           </div>
           <div className="row">
             <Button handleClick={this.addDecimal}>.</Button>
             <Button handleClick={this.addZeroToInput}>0</Button>
-            <Button handleClick={this.addToInput}>=</Button>
-            <Button handleClick={this.addToInput}>-</Button>
+            <Button handleClick={this.evaluate}>=</Button>
+            <Button handleClick={this.subtract}>-</Button>
           </div>
           <div className="row">
             <ClearButton handleClear={this.clearInput}>Clear</ClearButton>
